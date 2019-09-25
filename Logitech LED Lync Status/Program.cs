346b6fa0752fd3030d894e797e7e06dfba15f14c
Christopher Ure <christopher.ure@docuware.com>
2019-09-25 04:10:50 -0400

    -- Removed unused namespaces. -- Added some error handling to lyncClient initialization. -- Added sign in/out compatability. -- Renamed some event handlers. -- Fixed null reference bug when signing out. -- Added handling for DoNotDisturb and TemporarilyAway statuses.

--- Logitech LED Lync Status/Program.cs
+++ Logitech LED Lync Status/Program.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using Microsoft.Lync.Controls;
-using Microsoft.Lync.Controls.Framework;
 using Microsoft.Lync.Model;
 using Microsoft.Lync.Model.Conversation;
 using Microsoft.Lync.Model.Conversation.AudioVideo;
 using Microsoft.Lync.Utilities;
 using LedCSharp;
+using System.Threading;
 
 namespace Logitech_LED_Lync_Status
 {
     static class Program
     {
-        static LyncClient lyncClient = LyncClient.GetClient();
-        static Contact self = lyncClient.Self.Contact;
+        static LyncClient lyncClient;
+        static Self self;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -25,35 +24,61 @@ namespace Logitech_LED_Lync_Status
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            try
+            {
+                lyncClient = LyncClient.GetClient();
+            }
+            catch (ClientNotFoundException)
+            {
+                var result = MessageBox.Show("Skype for Business is not running!", "Warning", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+                if (result == DialogResult.Retry) Application.Restart();
+                else Environment.Exit(1);
+            }
+
             using (NotifyIcon icon = new NotifyIcon())
             {
                 icon.Icon = Properties.Resources.Icon1;
                 icon.ContextMenu = new ContextMenu(new MenuItem[] {
-                new MenuItem("Show form", (s, e) => {new Form1().Show();}),
                 new MenuItem("Exit", (s, e) => { Application.Exit(); }),
             });
                 icon.Visible = true;
 
-                LogitechGSDK.LogiLedInit();
-
-                self.ContactInformationChanged += OwnStatusHasChanged;
-                lyncClient.ConversationManager.ConversationAdded += ConversationManager_ConversationAdded;
-                lyncClient.ConversationManager.ConversationRemoved += ConversationManager_ConversationRemoved;
-                SetHeadsetLEDs((ContactAvailability)self.GetContactInformation(ContactInformationType.Availability), null);
-
+                
+                lyncClient.StateChanged += LyncClient_StateChanged;
+                ManualResetEventSlim _isRunning;
                 Application.Run();
-
-                LogitechGSDK.LogiLedShutdown();
                 icon.Visible = false;
             }
         }
 
-        private static void ConversationManager_ConversationRemoved(object sender, ConversationManagerEventArgs e)
+        private static void LyncClient_StateChanged(object sender, ClientStateChangedEventArgs e)
+        {
+            switch (e.NewState)
+            {
+                case ClientState.SignedIn:
+                    LogitechGSDK.LogiLedInit();
+                    self = lyncClient.Self;
+                    self.Contact.ContactInformationChanged += OwnInfoHasChanged;
+                    lyncClient.ConversationManager.ConversationAdded += ConversationAdded;
+                    lyncClient.ConversationManager.ConversationRemoved += ConversationRemoved;
+                    SetHeadsetLEDs((ContactAvailability)self.Contact.GetContactInformation(ContactInformationType.Availability));
+                    break;
+                case ClientState.SigningOut:
+                    self = null;
+                    LogitechGSDK.LogiLedShutdown();
+                    break;
+                case ClientState.ShuttingDown:
+                    break;
+            }         
+        }
+
+        private static void ConversationRemoved(object sender, ConversationManagerEventArgs e)
         {
             LogitechGSDK.LogiLedStopEffects();
         }
 
-        private static void ConversationManager_ConversationAdded(object sender, ConversationManagerEventArgs e)
+        private static void ConversationAdded(object sender, ConversationManagerEventArgs e)
         {
             var avModality = e.Conversation.Modalities[ModalityTypes.AudioVideo];
             if (avModality.State == ModalityState.Notified)
@@ -76,26 +101,29 @@ namespace Logitech_LED_Lync_Status
             }
         }
 
-        private static void OwnStatusHasChanged(object sender, ContactInformationChangedEventArgs e)
+        private static void OwnInfoHasChanged(object sender, ContactInformationChangedEventArgs e)
         {
-            if (!(e.ChangedContactInformation.Contains(ContactInformationType.Availability) || e.ChangedContactInformation.Contains(ContactInformationType.ActivityId))) return;
-
-            SetHeadsetLEDs((ContactAvailability)self.GetContactInformation(ContactInformationType.Availability), (string)self.GetContactInformation(ContactInformationType.ActivityId));
+            if (self != null && e.ChangedContactInformation.Contains(ContactInformationType.Availability))
+                SetHeadsetLEDs((ContactAvailability)self.Contact.GetContactInformation(ContactInformationType.Availability));
         }
 
-        private static void SetHeadsetLEDs(ContactAvailability availability, string activity)
+        private static void SetHeadsetLEDs(ContactAvailability availability)
         {
             switch (availability)
             {
+                case ContactAvailability.DoNotDisturb:
+                    LogitechGSDK.LogiLedSetLighting(100, 0, 100);
+                    break;
                 case ContactAvailability.Free:
                     LogitechGSDK.LogiLedSetLighting(0, 100, 0);
                     break;
                 case ContactAvailability.Busy:
-                case ContactAvailability.BusyIdle:
                     LogitechGSDK.LogiLedSetLighting(100, 0, 0);
                     break;
                 case ContactAvailability.Away:
                 case ContactAvailability.FreeIdle:
+                case ContactAvailability.BusyIdle:
+                case ContactAvailability.TemporarilyAway:
                     LogitechGSDK.LogiLedSetLighting(100, 75, 0);
                     break;
             }
